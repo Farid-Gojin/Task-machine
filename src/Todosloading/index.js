aa6087b91c4ec8c0a6683273ac6d811e7aa2992d
kuro <kuronosk8gs@gmail.com>
2024-03-20 21:06:12 -0500

    agujero negro loading

--- src/Todosloading/index.js
+++ src/Todosloading/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./Todosloading.css";
 
 function Todosloading() {
-  return <p className="Todosloading">loading .....</p>;
+  return (
+    <div className="container">
+      <span>Loading...</span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
 }
 
 export { Todosloading };
